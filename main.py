10636d4032671fe0a03a9b2200f4b21353b76042
luc <luc@appsflyer.com>
2019-09-28 12:46:27 +0200

    lots of refactoring, enum for providers, added frontend providers, sending only error logs to slack

--- main.py
+++ main.py
@@ -55,12 +55,14 @@ if __name__ == '__main__':
     cities = get_cities(settings)
     all_spls = []
     table_headers = ['City']
+    requests = 0
 
 
     for provider in list(Providers):
         if provider.value.frontend:
             all_spls.extend(provider.value.get_scooters())
             logging.info(f"{len(all_spls)} scooters retrieved from {provider.name}")
+            requests += 1
         else:
             table_headers.append(provider.name)
 
@@ -80,6 +82,7 @@ if __name__ == '__main__':
                 spls = provider.value.get_scooters(settings, city)
                 logging.info(f"{city.name}: {len(spls)} scooters retrieved from {provider.name}")
                 all_spls.extend(spls)
+                requests += 1
 
                 if len(spls) > 0:
                     city_log[provider.name] = len(spls)
@@ -96,9 +99,9 @@ if __name__ == '__main__':
             for provider in list(Providers):
                 if not provider.value.frontend:
                     log_row.append(city_log[provider.name])
-            log_rows.append(log_row)
-            if error:
-                error_rows.append(log_row)
+        log_rows.append(log_row)
+        if error:
+            error_rows.append(log_row)
 
 
     # Making a nice table summarizing results
@@ -107,21 +110,24 @@ if __name__ == '__main__':
     table.add_rows(log_rows)
     logging.info("\n" + table.draw())
 
+    ts = datetime.now().isoformat(sep=' ',timespec='seconds')
+    summary = f"Scooter Scrapper Result, run finish: {ts}\n\t{len(spls)} scooter position scrapped\n\t{requests} successful requests"
+
     if len(error_rows) > 1:
         error_table = Texttable()
         error_table.set_cols_align(["l", "c", "c", "c", "c", "c"])
         error_table.add_rows(error_rows)
-        logging.info("\n" + error_table.draw())
-        # sending the table to slack
-        if settings['SLACK']:
-            slack_connector.post_message(settings, "```" + table.draw() + "```")
+        summary += f"\n\t{len(error_rows) -1} cities with errors: \n```\n" + error_table.draw() + "\n```"
 
+    logging.info(summary)
 
+    if settings['SLACK']:
+        slack_connector.post_message(settings, summary)
 
     # Saving the data as a zipped CSV
     df = DataFrame(all_spls)
     df = DataFrame.from_records([s.to_dict() for s in all_spls])
-    ts = datetime.now().isoformat(sep=' ',timespec='seconds')
+
     df.to_csv (f'scrapped_data/{ts}_scooter_position_logs.csv.gz', header=True, index=False, compression='gzip')
 
     # Saving the data to Postgres
